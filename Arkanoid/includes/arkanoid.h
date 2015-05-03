49952e7d8cbd4a1ac926c1135aec59c42b009d65
Gaetan HILBERT <ghilbert@e1r11p4.42.fr>
2015-05-03 02:23:34 +0200

    Makefile update

--- Arkanoid/includes/arkanoid.h
+++ Arkanoid/includes/arkanoid.h
@@ -6,13 +6,13 @@
 /*   By: ghilbert <ghilbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/01 22:14:48 by lscopel           #+#    #+#             */
-/*   Updated: 2015/05/03 00:53:49 by ghilbert         ###   ########.fr       */
+/*   Updated: 2015/05/03 02:05:55 by ghilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARKANOID_H
 # define ARKANOID_H
-# include "../glfw/include/GLFW/glfw3.h"
+# include <GLFW/glfw3.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
